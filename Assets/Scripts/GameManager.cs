9b03522d8607aa270e5c262fd86adf788f32baad
sanandmv7 <56066399+sanandmv7@users.noreply.github.com>
2022-03-11 13:13:26 +0530

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { get; private set; }
+    public InputController inputController { get; private set; }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+        inputController = GetComponentInChildren<InputController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
