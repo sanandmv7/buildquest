964eea8966bfed537690af58054d51c76ffd303c
sanandmv7 <56066399+sanandmv7@users.noreply.github.com>
2022-03-12 17:09:11 +0530

    Blockchain integration

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using MoralisWeb3ApiSdk;
+using Moralis.Platform.Objects;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance { get; private set; }
     public InputController inputController { get; private set; }
+    [SerializeField] TextMesh playerWalletAddress;
 
     // Start is called before the first frame update
     void Awake()
     {
         instance = this;
         inputController = GetComponentInChildren<InputController>();
+        SetPlayerAddress();
+    }
+
+    private async void SetPlayerAddress(){
+        var user = await MoralisInterface.GetUserAsync();
+        if (user != null)
+        {
+            string addr = user.authData["moralisEth"]["id"].ToString();
+            playerWalletAddress.text = string.Format("{0}...{1}", addr.Substring(0, 6), addr.Substring(addr.Length - 3, 3));
+            playerWalletAddress.gameObject.SetActive(true);
+        }
     }
 
     // Update is called once per frame
